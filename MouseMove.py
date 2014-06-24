69cf66ed85f06329f7faa24ad656ea1b6c60d8aa
ledkk <bzhengbupt@gmail.com>
2014-06-24 18:56:56 +0800

    添加python控制鼠标移动，点击示例

---
+++ MouseMove.py
@@ -0,0 +1,18 @@
+﻿from pymouse import PyMouse
+
+'''
+windows 下需要安装
+    python for windows extensions http://sourceforge.net/projects/pywin32/files/
+    pyhook  http://www.lfd.uci.edu/~gohlke/pythonlibs/#pyhook
+    pymouse
+    注意选取的版本需要和安装的python的版本一致
+'''
+
+x = 100
+y = 100
+
+m = PyMouse()
+m.position() # 获取当前坐标的位置
+m.move(x,y) # 移动鼠标到x ， y 位置
+m.click(x,y) # 移动并在x y 位置处点击
+m.click(x,y,1|2) # 移动并且在xy 位置点击，左右键点击
